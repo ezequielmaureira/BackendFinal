ec3bf2192893b9e2b63e32416c0a0a44ad33f4c0
Ezequiel Maureira <ezequiel.maureira@gmail.com>
2022-07-19 19:54:27 -0300

    make it better

--- src/main/java/Backend/Backend/Controller/CompetenciaController.java
+++ src/main/java/Backend/Backend/Controller/CompetenciaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="https://porfolioezequiel.firebaseapp.com")
 
 public class CompetenciaController {
 
