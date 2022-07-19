163206c3e6980a5ee50fe8b6efb15268b1f85b14
Ezequiel Maureira <ezequiel.maureira@gmail.com>
2022-07-18 21:04:51 -0300

    vuelvo a 4200

--- src/main/java/Backend/Backend/Controller/CompetenciaController.java
+++ src/main/java/Backend/Backend/Controller/CompetenciaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins="https://porfolioezequiel.firebaseapp.com/")
+@CrossOrigin(origins="http://localhost:4200")
 
 public class CompetenciaController {
 
