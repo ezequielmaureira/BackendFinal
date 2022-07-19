4bcb82e0b52189017212489aa7cd702c729e692b
Ezequiel Maureira <ezequiel.maureira@gmail.com>
2022-07-19 20:15:25 -0300

    probando app

--- src/main/java/Backend/Backend/Controller/CompetenciaController.java
+++ src/main/java/Backend/Backend/Controller/CompetenciaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins="http://www.porfolioezequiel.firebaseapp.com")
+@CrossOrigin(origins="https://porfolioezequiel.web.app")
 
 public class CompetenciaController {
 
--- src/main/java/Backend/Backend/Controller/UsuarioDatosController.java
+++ src/main/java/Backend/Backend/Controller/UsuarioDatosController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins="https://porfolioezequiel.web.app")
 
+
 public class UsuarioDatosController {
 
    @Autowired
